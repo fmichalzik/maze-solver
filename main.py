784d4b6f05f95ab52a54a1838d46d606ab92bb14
fmichalzik <51121813+fmichalzik@users.noreply.github.com>
2025-05-22 07:40:24 +0200

    added line drawing and git ignore

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from window import Window
+from window import Window, Point, Line
 
 def main():
     win = Window(800, 600)
-    win.wait_for_close()
 
+    test_line = Line(Point(50, 50), Point(400, 400))
+
+    win.draw_line(test_line)
+    win.wait_for_close()
 main()
