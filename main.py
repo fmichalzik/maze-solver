0db324833feaff0aee5b89a70d3e6daace915a73
fmichalzik <51121813+fmichalzik@users.noreply.github.com>
2025-05-24 11:20:17 +0200

    added draw move

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from window import Window, Point, Line
+from window import Window, Point, Line, Cell
 
 def main():
     win = Window(800, 600)
 
-    test_line = Line(Point(50, 50), Point(400, 400))
+    """ test_line = Line(Point(50, 50), Point(400, 400))
 
-    win.draw_line(test_line)
+    win.draw_line(test_line) """
+
+    test_cell_a = Cell(win)
+    test_cell_b = Cell(win)
+    test_cell_c = Cell(win)
+    test_cell_a.draw(100, 150, 100, 150)
+    test_cell_b.draw(200, 250, 200, 250)
+    test_cell_c.draw(400, 450, 100, 150)
+
+    test_cell_a.draw_move(test_cell_c)
+    test_cell_c.draw_move(test_cell_b)
+
     win.wait_for_close()
 main()
--- window.py
+++ window.py
@@ -42,3 +42,45 @@ class Line:
             self.point_b.x, self.point_b.y,
             fill = fill_color, width = 2
         )
+
+class Cell:
+    def __init__(self, window):
+        self.has_left_wall = True
+        self.has_right_wall = True 
+        self.has_top_wall = True   
+        self.has_bottom_wall = True
+        self.__x1 = -1
+        self.__x2 = -1
+        self.__y1 = -1
+        self.__y2 = -1
+        self.__win = window
+    
+    def draw(self, x1, x2, y1, y2):
+        self.__x1 = x1
+        self.__x2 = x2
+        self.__y1 = y1
+        self.__y2 = y2
+
+        if self.has_left_wall:
+            left_wall = Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2))
+            self.__win.draw_line(left_wall)
+
+        if self.has_right_wall:
+            right_wall = Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2))
+            self.__win.draw_line(right_wall)
+        
+        if self.has_top_wall:
+            top_wall = Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1))
+            self.__win.draw_line(top_wall)
+
+        if self.has_bottom_wall:
+            bottom_wall = Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2))
+            self.__win.draw_line(bottom_wall)
+
+    def draw_move(self, to_cell, undo=False):
+        line = Line(Point(self.__x1 + ((self.__x2 - self.__x1) / 2), self.__y1 + ((self.__y2 - self.__y1) / 2)),
+                     Point(to_cell.__x1 + ((to_cell.__x2 - to_cell.__x1) / 2), to_cell.__y1 + ((to_cell.__y2 - to_cell.__y1) / 2)))
+        if not undo:
+            self.__win.draw_line(line, "red")
+            return
+        self.__win.draw_line(line, "gray")
